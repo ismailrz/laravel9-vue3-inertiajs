a0cf24f4b9eb1401bb3bcbca451ccfd890e1d86f
Md Ismail <ismailcse14@gmail.com>
2023-03-30 18:57:47 +0600

    Passing data to pages

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -3,8 +3,14 @@
     <div>
         <p>Index</p>
         <Link href="/show">Show</Link>
+        <p>This message is : {{ message }}</p>
     </div>
 </template>
 <script setup>
+
 import { Link } from "@inertiajs/vue3"
+defineProps({
+    message: String
+})
+
 </script>
